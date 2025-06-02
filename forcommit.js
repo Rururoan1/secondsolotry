0ee3635e89a7853a5edfd1fb267d1cb958ea417d
Rururoan1 <166687335+Rururoan1@users.noreply.github.com>
2025-06-02 09:03:18 +0800

    2nd commit

--- forcommit.js
+++ forcommit.js
@@ -1,1 +1,3 @@
-console.log('This is a commit message for the forcommit.js file.');
+console.log('This is a commit message for the forcommit.js file.');             
+
+console.log('This is my secondcommit message for the forcommit.js files.');
